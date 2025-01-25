c916ff3b578bbceef34f480c49600d36e5b6af37
kishoww <kishorear.personal@gmail.com>
2025-01-25 17:47:20 +0530

    Heading completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { React, useState} from 'react'
+import Search from './Components/Search.jsx'
 
 const App = () => {
+
+  const [SearchTerm, setSearch] = useState("")
+
+
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <main>
+
+      <div className='pattern' />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="hero-banner" srcset="" />
+          <h1>Find <span className='text-gradient'>Movies</span> you'll enjoy without Hassle</h1>
+          <p>Search</p>
+        </header>
+
+        <Search SearchTerm={SearchTerm} setSearch = {setSearch}/>
+
+      </div>
+
+    </main>
   )
 }
 
---
+++ src/Components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Search = (props) => {
+  return (
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt="search" />
+
+        <input 
+            type='text'
+            placeholder='Search through thousands of movies..'
+            value={props.SearchTerm}
+            onChange={(event) => props.setSearch(event.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search
